8604fe0cc1d91880dca967daf3b9269e7d2c51e8
David P. Bauer <dbauer1@udayton.edu>
2018-02-21 11:15:16 -0500

    Revert "New flag for quick write"
    This reverts commit ab2a2c4c8f259184786654eb18242071714713d5.

--- register.php
+++ register.php
@@ -5,6 +5,5 @@ $REGISTER_LTI2 = array(
     "FontAwesome" => "fa-pencil-square", // Icon for the tool
     "short_name" => "Quick Write",
     "description" => "A simple tool to prompt users to respond to short answer questions.", // Tool description
-    "messages" => array("launch", "launch_grade"),
-    "tool_phase" => "beta"
+    "messages" => array("launch", "launch_grade")
 );
